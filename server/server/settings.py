70acf963b1da0de54e1b7291e8ae093e3598ee2f
Nikhil Soni <krsoninikhil@gmail.com>
2017-04-05 18:34:18 +0530

    Add static_root for heroku

--- server/server/settings.py
+++ server/server/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
