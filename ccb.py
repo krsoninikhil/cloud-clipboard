5b35334f2bec7604e10cb2a6ec24d5b3c67988c3
Nikhil Soni <krsoninikhil@gmail.com>
2017-04-05 17:36:11 +0530

    Update url

---
+++ ccb.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+# This script acts as desktop client
+
+import requests
+import os
+import sys
+import subprocess
+
+server_url = "https://cloudclipboard-1.herokuapp.com/" # "http://localhost:8000/"
+
+def copy():
+    """
+    Returns the current text on clipboard.
+    """
+    if os.name == "posix":
+        p = subprocess.Popen(["xsel", "-o"], stdout=subprocess.PIPE)
+        data = p.communicate()[0].decode("utf-8")
+    elif os.name == "nt":
+        data = None
+    else:
+        print("We don't yet support %s Operating System." % os.name)
+        exit()
+    return data
+
+def upload():
+    """
+    Sends the copied text to server.
+    """
+    payload = {"text": copy(), "device": ""}
+    res = requests.put(
+        server_url+"copy-paste/",
+        data = payload,
+        auth = ('nik', 'nik#RA.1')
+    )
+    if res.status_code == 200:
+        print("Succeses! Copied to Cloud-Clipboard.")
+    else:
+        print("Error:", res.text)
+
+def paste(data):
+    """
+    Copies 'data' to local clipboard which enables pasting.
+    """
+    p = subprocess.Popen(["xsel", "-bi"], stdout=subprocess.PIPE,
+                         stdin=subprocess.PIPE)
+    p = p.communicate(data.encode("utf-8"))
+    if p[1] is not None:
+        print("Error in accessing local clipboard")
+
+def download():
+    """
+    Downloads from server and updates the local clipboard.
+    """
+    res = requests.get(server_url+"copy-paste/", auth=('nik', 'nik#RA.1'))
+    if res.status_code == 200:
+        paste(res.text)
+    else:
+        print("Cannot download the data.")
+
+def usage():
+    print("Error: Unknown argument")
+    print("Usage: ccb.py copy|paste")
+    
+
+if __name__ == "__main__":
+    #print("Cloud Clipboard -- Share you clipboard accross the devices.")
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "copy":
+            upload()
+        elif sys.argv[1] == "paste":
+            download()
+        else:
+            usage()
+    else:
+        usage()
