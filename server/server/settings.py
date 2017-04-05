8fee3735bc8bdde218e6af88965c115f2599f9e7
Nikhil Soni <krsoninikhil@gmail.com>
2017-04-05 20:06:24 +0530

    Set up the production db

--- server/server/settings.py
+++ server/server/settings.py
@@ -83,6 +83,11 @@ DATABASES = {
     }
 }
 
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
